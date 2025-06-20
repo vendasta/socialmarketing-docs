3ebb1d68d937392b6b165180a6c3750e638ad1fa
Jesse Redl <jredl@vendasta.com>
2025-06-20 12:38:56 -0600

    Revert "Update documentation site URL to https://docs.socialsmbs.com"
    This reverts commit 783b9aaf5433f2e44deff857fb41e8190d844a8e.

--- docusaurus/docusaurus.config.ts
+++ docusaurus/docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://docs.socialsmbs.com',
+  url: 'https://docs.vendasta.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
