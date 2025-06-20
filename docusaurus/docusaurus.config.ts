783b9aaf5433f2e44deff857fb41e8190d844a8e
Jesse Redl <jredl@vendasta.com>
2025-06-20 12:37:11 -0600

    Update documentation site URL to https://docs.socialsmbs.com

--- docusaurus/docusaurus.config.ts
+++ docusaurus/docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://docs.vendasta.com',
+  url: 'https://docs.socialsmbs.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
