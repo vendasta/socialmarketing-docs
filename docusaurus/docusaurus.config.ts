a4998d6cd264ce90674adf0c5ed29b881a891121
Calista <calistacooper@vendasta.com>
2025-08-09 20:47:41 -0600

    Docs: make docs index the landing page, remove old homepage, add card-link CSS; update internal footer link to /getting-started/

--- docusaurus/docusaurus.config.ts
+++ docusaurus/docusaurus.config.ts
@@ -104,7 +104,7 @@ const config: Config = {
           items: [
             {
               label: 'Getting started',
-              to: 'https://docs.socialsmbs.com/getting-started/',
+              to: '/getting-started/',
             },
           ],
         },
